cc6af9d89ef6295ebc23c0ae88207df6781d3a92
조준민 <85844303+coom1222@users.noreply.github.com>
2026-04-09 10:49:40 +0900

    [Silver IV] Title: blobyum, Time: 720 ms, Memory: 114704 KB -BaekjoonHub

--- 백준/Silver/24499. blobyum/blobyum.java
+++ 백준/Silver/24499. blobyum/blobyum.java
@@ -1,48 +1,48 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        // 애플파이의 개수 N과 먹을 개수 K 공백으로 입력받기
-        int N = scanner.nextInt();  // 5
-        int K = scanner.nextInt();  // 3
-        int maxSum = 0;
+        // N개의 애플파이
+        int N = sc.nextInt();
 
-        int[] Pies = new int[N];
+        // 연속으로 배치된 K를 먹을거다
+        int K = sc.nextInt();
+
+        // 맛있는 정도를 나타내는 값인 A_n이 공백으로 구분되어 주어진다.
+        int[] deliciousA = new int[N];
         for (int i = 0; i < N; i++) {
-            Pies[i] = scanner.nextInt();
+            deliciousA[i] = sc.nextInt();
         }
 
-        // 1. 원형 배열 문제를 해결하기 위해 배열을 확장 (기존 배열 뒤에 맨 앞 K-1개를 이어붙임)
-        int[] extendedPies = new int[N + K - 1];
-        for (int i = 0; i < N; i++) {
-            extendedPies[i] = Pies[i];
-        }
-        for (int i = 0; i < K - 1; i++) {
-            extendedPies[N + i] = Pies[i];
+        // 배열 복사
+        int[] deliciousB = Arrays.copyOf(deliciousA, 2*deliciousA.length);
+        for (int i = N; i < deliciousB.length-1; i++) {
+            deliciousB[i] = deliciousA[i-N];
         }
 
-        // 2. 첫 번째 윈도우(0번부터 K-1번까지)의 합을 계산
-        int currentSum = 0;
+     
+
+        int tempMax = 0;
+        int Max = 0;
+        // 0부터 연속된 K개의 최대합 구하기 -> 배열을 슬라이딩 윈도우 형식으로 탐색해보자!
         for (int i = 0; i < K; i++) {
-            currentSum += extendedPies[i];
-            maxSum = currentSum;
+            tempMax += deliciousA[i];
+            Max = tempMax;
         }
 
-        // 3. 슬라이딩 윈도우 진행 (K번째 요소부터 확장된 배열의 끝까지)
-        for (int i = K; i < extendedPies.length; i++) {
-            // 맨 왼쪽 값(i-K)을 빼고, 새로 들어온 오른쪽 값(i)을 더함
-            currentSum = currentSum - extendedPies[i - K] + extendedPies[i];
+        // 하나씩 옮겨가기
+        for (int i = K; i < N + K - 1; i++) {
+            tempMax = tempMax - deliciousB[i-K] + deliciousB[i];
 
-            // maxSum 갱신
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
+            if(tempMax > Max){
+                Max = tempMax;
             }
         }
 
-        System.out.println(maxSum);
-        scanner.close();
+        System.out.println(Max);
     }
 }
