451d38d3d82d438c82d9011ae18d6d6b76a0d1a5
조준민 <85844303+coom1222@users.noreply.github.com>
2026-07-23 01:53:47 +0900

    [Passed] 10개 입력받기 (127ms, 10MB)

---
+++ trail1/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[10];
+        int sum = 0;
+        double avg = 0;
+        int count = 0;
+
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+
+            if(arr[i] == 0) {break;}
+
+            sum += arr[i];
+            count++;
+        }
+
+        avg = (double) sum / count;
+
+    System.out.printf("%d %.1f", sum, avg);
+
+    }
+}
