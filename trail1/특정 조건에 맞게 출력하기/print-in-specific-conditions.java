6f252552adb895177d2121e09410ce79c208d7b0
조준민 <85844303+coom1222@users.noreply.github.com>
2026-08-11 10:15:07 +0900

    [Passed] 특정 조건에 맞게 출력하기 (131ms, 10MB)

---
+++ trail1/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 100; i++){
+            int n = sc.nextInt();
+            if (n == 0) break;
+
+            if(n%2 == 0) {
+                System.out.printf("%d ", n/2);
+            } else {
+                System.out.printf("%d ", n+3);
+            }
+        }
+        
+
+
+    }
+}
