037cce4f3657fe0a07dc8421ab884cad372ba997
조준민 <85844303+coom1222@users.noreply.github.com>
2026-08-16 22:06:17 +0900

    [Passed] 특정 위치의 문자 (123ms, 10MB)

---
+++ trail1/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] word = new char[]{ 'L', 'E', 'B', 'R', 'O', 'S' };
+
+        // 해당 문자를 찾지 못했다면 -1
+        int idx = -1;
+
+        char ch = sc.next().charAt(0);
+
+        // 문자 탐색
+        for (int i = 0; i < 6; i++) {
+            if (word[i] == ch) {
+                idx = i;
+            }
+        }
+
+        // 문자가 존재하지 않는 경우
+        if (idx == -1) {
+            System.out.println("None");
+        }
+        else {
+            System.out.println(idx);
+        }
+
+    }
+}
