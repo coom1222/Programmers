b770fa3feb94174a8613d5b6065faddc41336751
조준민 <85844303+coom1222@users.noreply.github.com>
2026-09-16 22:13:04 +0900

    [Passed] 배열의 합 (2ms, 0MB)

---
+++ trail1/배열의 합/sum-of-array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[4][4];
+    
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            scanf("%d ", &arr[i][j]);
+        }
+
+    }
+
+    
+    for(int i = 0; i < 4; i++){
+        int sum = 0;
+        for(int j = 0; j < 4; j++){
+            sum += arr[i][j];
+        }
+        printf("%d\n", sum);
+        
+    }
+    
+
+
+    return 0;
+}
